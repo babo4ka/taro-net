1f5753cfa5c8289506445083fead3707a5ea3d8f
basra4ka <evgeha_ivanov_01@bk.ru>
2024-01-03 13:52:45 +0300

    device

--- net/taro_nets_utils.py
+++ net/taro_nets_utils.py
@@ -94,6 +94,8 @@ def get_batch(seq, batch_size):
 
 def generate_text(net, words_to_indexes, indexes_to_words, device, start_text='карта указывает', pred_len=50, temp=0.3):
     hidden = net.init_hidden()
+    hidden[0].to(device)
+    hidden[1].to(device)
     idx_inp = [words_to_indexes[str] for str in start_text.split(' ')]
     train = torch.LongTensor(idx_inp).view(-1, 1, 1).to(device)
     pred_text = start_text
